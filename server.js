51da3d647e36c1e1c57665f1fd6f20b435111db1
Elliott Payne <e.payne000@gmail.com>
2020-11-28 12:40:26 -0600

    react setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
 
 app.listen(PORT, function() {
